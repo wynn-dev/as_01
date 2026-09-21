2725670f844267d346013c0e15d297777ffa25d8
wynn-dev <wynn@wynn.website>
2026-09-21 15:02:25 +0200

    Upload A3

---
+++ A3/decoder.s
@@ -0,0 +1,40 @@
+.text
+
+.include "helloWorld.s"
+
+.global main
+
+# ************************************************************
+# Subroutine: decode                                         *
+# Description: decodes message as defined in Assignment 3    *
+#   - 2 byte unknown                                         *
+#   - 4 byte index                                           *
+#   - 1 byte amount                                          *
+#   - 1 byte character                                       *
+# Parameters:                                                *
+#   first: the address of the message to read                *
+#   return: no return value                                  *
+# ************************************************************
+decode:
+	# prologue
+	pushq	%rbp 			# push the base pointer (and align the stack)
+	movq	%rsp, %rbp		# copy stack pointer value to base pointer
+
+	# your code goes here
+
+	# epilogue
+	movq	%rbp, %rsp		# clear local variables from stack
+	popq	%rbp			# restore base pointer location 
+	ret
+
+main:
+	pushq	%rbp 			# push the base pointer (and align the stack)
+	movq	%rsp, %rbp		# copy stack pointer value to base pointer
+
+	movq	$MESSAGE, %rdi	# first parameter: address of the message
+	call	decode			# call decode
+
+	popq	%rbp			# restore base pointer location 
+	movq	$0, %rdi		# load program exit code
+	call	exit			# exit the program
+
